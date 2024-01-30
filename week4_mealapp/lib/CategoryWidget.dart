e6faaaf4367863f8706d64dc5656cde204ffe3dc
rania <raniaarbash@hotmail.com>
2024-01-30 10:14:50 -0500

    V3

--- week4_mealapp/lib/CategoryWidget.dart
+++ week4_mealapp/lib/CategoryWidget.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:week4_mealapp/Category.dart';
+import 'package:week4_mealapp/CategoryMeals.dart';
 
 class CategoryItem extends StatelessWidget {
-  final String cTitle;
-  final Color cColor;
-  final String cId;
+  final Category category;
 
-  const CategoryItem(this.cId, this.cTitle, this.cColor);
+  const CategoryItem(this.category);
+
+  void selectCategory(BuildContext context, Category category) {
+    // Navigator.of(context).push(route)
+
+    Navigator.push(context, MaterialPageRoute(builder: (_) {
+      return CategoryMeals(category: category);
+    }));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: 40,
-        height: 40,
-        decoration: BoxDecoration(
-            color: cColor, borderRadius: BorderRadius.circular(15)),
-        child: Text(
-          cTitle,
-        ));
+    return InkWell(
+      onTap: () {
+        selectCategory(context, category);
+      },
+      child: Container(
+          padding: const EdgeInsets.all(25),
+          decoration: BoxDecoration(
+              color: category.color, borderRadius: BorderRadius.circular(15)),
+          child: Text(
+            category.title,
+            style: const TextStyle(fontSize: 15),
+          )),
+    );
   }
 }
--- week4_mealapp/lib/main.dart
+++ week4_mealapp/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:week4_mealapp/CategoryWidget.dart';
 import 'package:week4_mealapp/DummyData.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(const MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,17 +15,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Meal App")),
-      body: Expanded(
-        child: GridView(
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 30,
-              mainAxisExtent: 30),
-          children: DUMMY_CATEGORIES
-              .map((cat) => CategoryItem(cat.id, cat.title, cat.color))
-              .toList(),
-        ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 100,
+            mainAxisExtent: 100,
+            mainAxisSpacing: 20),
+        children: DUMMY_CATEGORIES.map((cat) => CategoryItem(cat)).toList(),
       ),
     );
   }
