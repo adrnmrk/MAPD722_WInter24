6095021b1e1df6d72292f41bde3c91888ca26ed9
rania <raniaarbash@hotmail.com>
2024-02-06 10:04:14 -0500

    Week5-AddingArabicLanguage

--- week4_mealapp/lib/main.dart
+++ week4_mealapp/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:week4_mealapp/CategoryWidget.dart';
-import 'package:week4_mealapp/DummyData.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:week4_mealapp/CategoryList.dart';
+import 'package:week4_mealapp/FilterScreen.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
-  runApp(const MaterialApp(home: MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,17 +15,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Meal App")),
-      body: GridView(
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 100,
-            mainAxisExtent: 100,
-            mainAxisSpacing: 20),
-        children: DUMMY_CATEGORIES.map((cat) => CategoryItem(cat)).toList(),
-      ),
+    return MaterialApp(
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('ar'), // Arabic
+      ],
+      routes: {
+        '/': (context) => const CategoryList(),
+        '/category-meals': (context) => const CategoryList(),
+        '/filters-screen': (context) => FilterScreen()
+      },
     );
   }
 }
